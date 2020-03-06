5d8bb83e82d054b72b6e8a95b83631bcd6bc3c1c
Corentin Dupret <corentin.dupret@student.ecp.fr>
2020-03-06 11:31:31 +0100

    :white_check_mark: Update coordinates convention

--- test_game.py
+++ test_game.py
@@ -24,32 +24,32 @@ def test_apply_move():
     # H:10#W:10#____#____#
     ######################
     grid = {
-        Coordinates(0, 0): Cell(HUMAN, 5),
-        Coordinates(1, 0): Cell(VAMPIRE, 6),
-        Coordinates(2, 0): Cell(HUMAN, 10),
+        Coordinates(0, 2): Cell(HUMAN, 5),
+        Coordinates(0, 1): Cell(VAMPIRE, 6),
+        Coordinates(0, 0): Cell(HUMAN, 10),
         Coordinates(1, 1): Cell(WEREWOLF, 17),
-        Coordinates(2, 1): Cell(WEREWOLF, 10),
-        Coordinates(1, 3): Cell(VAMPIRE, 1),
+        Coordinates(1, 0): Cell(WEREWOLF, 10),
+        Coordinates(3, 1): Cell(VAMPIRE, 1),
     }
     state = State(grid=grid, height=h, width=w)
     with pytest.raises(IllegalMoveException):
-        state.apply_moves([Move(Coordinates(1, 0), 7, Coordinates(0, 0),)], VAMPIRE)
-        assert state.get_cell(1, 0) == Cell(VAMPIRE, 6)
+        state.apply_moves([Move(Coordinates(0, 1), 7, Coordinates(0, 2),)], VAMPIRE)
+        assert state.get_cell(0, 1) == Cell(VAMPIRE, 6)
     with pytest.raises(IllegalMoveException):
-        state.apply_moves([Move(Coordinates(1, 0), 6, Coordinates(0, 0),)], WEREWOLF)
-        assert state.get_cell(1, 0) == Cell(VAMPIRE, 6)
-    state.apply_moves([Move(Coordinates(1, 0), 6, Coordinates(0, 0),)], VAMPIRE)
+        state.apply_moves([Move(Coordinates(0, 1), 6, Coordinates(0, 2),)], WEREWOLF)
+        assert state.get_cell(0, 1) == Cell(VAMPIRE, 6)
+    state.apply_moves([Move(Coordinates(0, 1), 6, Coordinates(0, 2),)], VAMPIRE)
+    assert state.get_cell(0, 1) is None
+    assert state.get_cell(0, 2) == Cell(VAMPIRE, 11)
+    state.apply_moves([Move(Coordinates(1, 1), 17, Coordinates(0, 2),)], WEREWOLF)
+    assert state.get_cell(0, 1) is None
+    assert state.get_cell(0, 2) == Cell(WEREWOLF, 17)
+    state.apply_moves([Move(Coordinates(3, 1), 1, Coordinates(3, 0),)], VAMPIRE)
+    assert state.get_cell(3, 1) is None
+    assert state.get_cell(3, 0) == Cell(VAMPIRE, 1)
+    state.apply_moves([Move(Coordinates(1, 0), 10, Coordinates(0, 0),)], WEREWOLF)
     assert state.get_cell(1, 0) is None
-    assert state.get_cell(0, 0) == Cell(VAMPIRE, 11)
-    state.apply_moves([Move(Coordinates(1, 1), 17, Coordinates(0, 0),)], WEREWOLF)
-    assert state.get_cell(1, 0) is None
-    assert state.get_cell(0, 0) == Cell(WEREWOLF, 17)
-    state.apply_moves([Move(Coordinates(1, 3), 1, Coordinates(2, 3),)], VAMPIRE)
-    assert state.get_cell(1, 3) is None
-    assert state.get_cell(2, 3) == Cell(VAMPIRE, 1)
-    state.apply_moves([Move(Coordinates(2, 1), 10, Coordinates(2, 0),)], WEREWOLF)
-    assert state.get_cell(2, 1) is None
-    cell = state.get_cell(2, 0)
+    cell = state.get_cell(0, 0)
     if cell is not None:
         assert cell.race in (WEREWOLF, HUMAN)
         assert 1 <= cell.count <= 20
